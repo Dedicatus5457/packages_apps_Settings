LeafOS-Project/android_packages_apps_Settings

Settings: Add high touch sensitivity toggle
* Like https://review.lineageos.org/c/LineageOS/android_packages_apps_Settings/+/321581
  but in an AOSP compatible way

Change-Id: Ib8712d2d0c0fe4396dbea7c7fc130e0c253d3207

src/org/leafos/settings/BootCompletedReceiver.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (c) 2022 The LeafOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.leafos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.android.settings.display.HighTouchSensitivityPreferenceController;
+import static com.android.settings.display.HighTouchSensitivityPreferenceController.GLOVE_MODE_ENABLE;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (Settings.System.getInt(context.getContentResolver(), GLOVE_MODE_ENABLE, 0) != 0) {
+            HighTouchSensitivityPreferenceController.enableStatic(true);
+        }
+    }
+
+}

Dedicatus5457/packages_apps_Settings

src/org/derpfest/settings/BootCompletedReceiver.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2022 The LeafOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import org.derpfest.settings.display.HighTouchSensitivityPreferenceController;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (Settings.System.getInt(context.getContentResolver(), Settings.System.HIGH_TOUCH_SENSITIVITY_ENABLE, 0) != 0) {
+            HighTouchSensitivityPreferenceController.enableStatic(true);
+        }
+    }
+
+}